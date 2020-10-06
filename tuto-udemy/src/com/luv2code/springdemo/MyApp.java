37f0fd8e34f5e3bf3b2c55facd12ec5444056a76
moussa <m.sow@deal.fr>
2020-10-06 10:09:27 +0200

    scope and lifecycle for bean

--- tuto-udemy/src/com/luv2code/springdemo/MyApp.java
+++ tuto-udemy/src/com/luv2code/springdemo/MyApp.java
@@ -1,14 +1,33 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
 
 	public static void main(String[] args) {
-		// create object 
-		TrackCoach theCoach = new TrackCoach();
+		//load  spring configuration file
+		ClassPathXmlApplicationContext context = new
+				ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		// use object 
-		System.out.println(theCoach.getDailyWorkout());
+		// retrieve bean from spring container
+		Coach theCoach0 = context.getBean("myCoach", Coach.class);
+		Coach theCoach1 = context.getBean("myCoach", Coach.class);
 
+		Boolean isSameReference = theCoach0 == theCoach1;
+		System.out.println("is the same reference ? " + isSameReference);	
+		System.out.println("what about the theCoach0 ? " + theCoach0);	
+		System.out.println("what about the theCoach1 ? " + theCoach1);
+		/*
+		 * in case we set in the applicationContext.xml file 
+		 * the scope to singleton or not set it (the default value)
+		 * the reference not be equal=> isSameReference == true
+		 * otherwise it will be true
+		 * */
+		
+		
+		
+		// close context
+		context.close();
 	}
 
 }
--- tuto-udemy/src/com/luv2code/springdemo/TrackCoach.java
+++ tuto-udemy/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,23 +1,29 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach{
-
-	public FortuneService fortuneService;
 	
 	public TrackCoach() {}
 	
-	public TrackCoach(FortuneService fortuneService) {
-		this.fortuneService = fortuneService;
-	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "TrackCoach => running for warm";
 	}
 
+
 	@Override
 	public String getDailyFortune() {
-		return fortuneService.getFortune();
+		// TODO Auto-generated method stub
+		return null;
 	}
-
+	
+	// add init method (after constructor method call)
+	public void initMethod() {
+		System.out.println("init method call");
+	}
+	
+	// add destroy method (before bean destroy )
+	public void destroyMethod() {
+		System.out.println("destroy method call");
+	}
 }
