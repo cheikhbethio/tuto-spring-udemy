2d357643aa5d731e3ac30042d728cedc76edb303
moussa <m.sow@deal.fr>
2020-10-06 12:48:08 +0200

    qualifiers injection spring conf with java annotation

--- tuto-udemy/src/com/luv2code/springdemo/TrackCoach.java
+++ tuto-udemy/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,10 +1,14 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+// java 9 and later version use the javax.anotation-api-1.3.2.jar 
 @Component
-@Scope("prototype")
+@Scope()
 public class TrackCoach implements Coach{
 	
 	public TrackCoach() {}
@@ -22,4 +26,15 @@ public class TrackCoach implements Coach{
 		return null;
 	}
 	
+	// add init method (after constructor method call)
+	@PostConstruct
+	public void initMethod() {
+		System.out.println("init method call");
+	}
+	
+	// add destroy method (before bean destroy )
+	@PreDestroy
+	public void destroyMethod() {
+		System.out.println("destroy method call");
+	}
 }
